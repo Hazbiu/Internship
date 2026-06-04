347e972e60fee193fb6a26273872b5fab91ebf45
hazbiu <ketihazbiu@hotmail.com>
2026-06-04 21:20:58 +0200

    Updates

--- ros2_ws/build/cmdvel_to_ackermann/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hazbiu/ros2_ws/src/IsaacSim-ros_workspaces/jazzy_ws/src/ackermann_control/cmdvel_to_ackermann
-# Build directory: /home/hazbiu/ros2_ws/build/cmdvel_to_ackermann
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
